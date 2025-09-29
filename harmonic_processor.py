841b99ac2ecf1c7f5c13d1614213146566183175
Lucas Kuzma <lucas@lucaskuzma.com>
2025-09-29 06:57:53 +0200

    fix index scope

--- harmonic_processor.py
+++ harmonic_processor.py
@@ -13,17 +13,18 @@ class HarmonicProcessor:
             int(round(12 * math.log2(n), 2)) for n in range(1, n_harmonics + 1)
         ]
         self.automaton = CountingAutomaton()
+        self.index = 0
 
     def process(self, note):
-        index = self.automaton.step() % self.n_harmonics
-        delta = self.harmonics[index]
+        self.index = self.automaton.step() % self.n_harmonics
+        delta = self.harmonics[self.index]
         return delta + note
 
     def __str__(self):
         str_parts = []
         for i, h in enumerate(self.harmonics):
-            if i == index:
-                str_parts.append(f" {h:02d} ")
-            else:
+            if i == self.index:
                 str_parts.append(f"[{h:02d}]")
+            else:
+                str_parts.append(f" {h:02d} ")
         return "".join(str_parts)
